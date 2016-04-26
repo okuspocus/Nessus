aef7a12685aaaa8f517b6ebcce6f905ff15979e9
astals <marc.astals@gmail.com>
2016-04-26 19:18:19 +0200

    plots inprovement

--- R/plots.R
+++ R/plots.R
@@ -1,19 +1,18 @@
 
 portsPlot <- function(input_data){
-  input_data <- input_data[ input_data != 0 ]
-  print(table(input_data))
+  print(table(as.character(input_data)))
   colors <- c("firebrick1", "gold", "seagreen2", "dodgerblue3", "darkorange", "hotpink", "cadetblue2")
   pie(table(input_data), main="Port Vulneravility Distribution", col=colors)
 }
 
 vulnsByIp <- function(input_data){
+  colors <- c("firebrick1", "gold", "seagreen2", "dodgerblue3", "darkorange", "hotpink", "cadetblue2")
   barplot(table(input_data), main="Vulnerability Distribution By IP", col=colors,ylab="count", xlab = "severity")
 }
 
 severityDist <- function(input_data){
   input_data <- floor(as.numeric(input_data))
-  colnames<-c('0','1','2','3', '4', '5', '6', '7','8','9','10')
   paleta <- colorRampPalette(c("yellow","red"))
   colors <- paleta(10)
-  barplot(table(input_data), main="Vulnerability Severity Distribution", col=colors,ylab="count", xlab = "severity")
+  barplot(table(input_data), main="Vulnerability Severity Distribution", col=colors,ylab="count", xlab = "severity", )
 }
