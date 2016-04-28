bff7e7f0e616926b3c7b085d0db695241fd23ae0
Eloi <emanuel@wsg127.com>
2016-04-28 18:06:12 +0200

    Comments and improvements on the CVEs

--- merger.R
+++ merger.R
@@ -23,30 +23,26 @@ for (ip in IPs) {
                                stringsAsFactors = FALSE) #Get all the attrs
   data_frame_aux$svc_name <- NULL #Delete the svc_name attribute
   data_frame <- data.frame(cbind(IP = as.character(ip), data_frame_aux), stringsAsFactors = FALSE)
-  data_frame[,"IP"] <- as.character(data_frame[,"IP"])
+  data_frame[,"IP"] <- as.character(data_frame[,"IP"])    #Convert the IP column to srting
   data_frame <- data.frame(cbind(data_frame, CVE = as.character("")), stringsAsFactors = FALSE)
-  data_frame[,"CVE"] <- as.character(data_frame[,"CVE"])
+  data_frame[,"CVE"] <- as.character(data_frame[,"CVE"])  #Convert the CVE column to string
   
+  #Initialize and fill the df.cves dataframe
   df.cves <- data.frame(CVEs = as.character(), stringsAsFactors = FALSE)
   for (i in 1:nrow(data_frame_aux)) {
-    port <- data_frame_aux[i, c("port")]
-    protocol <- data_frame_aux[i, c("protocol")]
-    pluginID <- data_frame_aux[i, c("pluginID")]
+    port <- data_frame_aux[i, c("port")]          #Get the port
+    protocol <- data_frame_aux[i, c("protocol")]  #Get the protocol
+    pluginID <- data_frame_aux[i, c("pluginID")]  #Get the pluginID
+    
     xpath <- paste("//ReportHost[@name='", ip,
                    "']/ReportItem[@port='", port,
                    "' and @protocol='", protocol,
                    "' and @pluginID='", pluginID,
                    "']/cve", sep = "")
+    
     df.cves <- data.frame(CVEs = sapply(XML::xpathApply(doc, xpath), unlist(XML::xmlValue)), stringsAsFactors = FALSE)
-    #tempo <- cbind(IP = ip,
-    #               port = as.character(port),
-    #               protocol = as.character(protocol),
-    #               pluginID = as.character(pluginID),
-    #               CVEs = as.vector(df.cves[['CVEs']]))
-    if (nrow(df.cves) == 0) {
-      #data_frame <- as.data.frame(cbind(data_frame[i], as.vector("")), stringsAsFactors = FALSE)
-    }
-    else {
+    
+    if (nrow(df.cves) != 0) {
       data_frame[i, "CVE"] <- as.character(paste(as.character(df.cves[['CVEs']]), collapse = ", "))
     }
   }
