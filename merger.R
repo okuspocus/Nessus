1ef7bec04bef9c27e45c28c49b3a0e7e16f6edd8
Eloi <emanuel@wsg127.com>
2016-04-30 01:01:39 +0200

    Added CWEs

--- merger.R
+++ merger.R
@@ -29,22 +29,35 @@ for (ip in IPs) {
   
   #Initialize and fill the df.cves dataframe
   df.cves <- data.frame(CVEs = as.character(), stringsAsFactors = FALSE)
+  df.cwes <- data.frame(CWEs = as.character(), stringsAsFactors = FALSE)
   for (i in 1:nrow(data_frame_aux)) {
     port <- data_frame_aux[i, c("port")]          #Get the port
     protocol <- data_frame_aux[i, c("protocol")]  #Get the protocol
     pluginID <- data_frame_aux[i, c("pluginID")]  #Get the pluginID
     
-    xpath <- paste("//ReportHost[@name='", ip,
+    xpath_cves <- paste("//ReportHost[@name='", ip,
                    "']/ReportItem[@port='", port,
                    "' and @protocol='", protocol,
                    "' and @pluginID='", pluginID,
                    "']/cve", sep = "")
     
-    df.cves <- data.frame(CVEs = sapply(XML::xpathApply(doc, xpath), unlist(XML::xmlValue)), stringsAsFactors = FALSE)
+    df.cves <- data.frame(CVEs = sapply(XML::xpathApply(doc, xpath_cves), unlist(XML::xmlValue)), stringsAsFactors = FALSE)
     
     if (nrow(df.cves) != 0) {
       data_frame[i, "CVE"] <- as.character(paste(as.character(df.cves[['CVEs']]), collapse = ", "))
     }
+    
+    xpath_cwes <- paste("//ReportHost[@name='", ip,
+                        "']/ReportItem[@port='", port,
+                        "' and @protocol='", protocol,
+                        "' and @pluginID='", pluginID,
+                        "']/cwe", sep = "")
+    
+    df.cwes <- data.frame(CWEs = sapply(XML::xpathApply(doc, xpath_cwes), unlist(XML::xmlValue)), stringsAsFactors = FALSE)
+    if (nrow(df.cwes) != 0) {
+      data_frame[i, "CWE"] <- as.character(paste(as.character(df.cwes[['CWEs']]), collapse = ", "))
+    }
+    
   }
   
   df_global <- as.data.frame(rbind(df_global, data_frame))
